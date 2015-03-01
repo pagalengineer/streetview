a328536b48e6e4fd0b3ca042594a6221b8215e4b
pagalengineer <pagalengineer@users.noreply.github.com>
2015-03-01 20:45:49 +0530

    Create Slice.java
    Slice the Panorama into multiple images , change rows and column in the code as per the requirement.

---
+++ Slice.java
@@ -0,0 +1,64 @@
+package virtualtour;
+import javax.imageio.ImageIO;  
+import java.awt.image.BufferedImage;  
+import java.io.*;  
+import java.awt.*;  
+
+/*  Pagalengineer.com  */
+  
+public class slice {  
+    public static void main(String[] args) throws IOException {  
+  
+        File file = new File("D:/exif/images/test.jpg"); // I have bear.jpg in my working directory  
+        FileInputStream fis = new FileInputStream(file);  
+        BufferedImage image = ImageIO.read(fis); //reading the image file  
+  
+        int rows = 1; //You should decide the values for rows and cols variables  
+        int cols = 4;  
+        int chunks = rows * cols;  
+        String suffix="";
+  
+        int chunkWidth = image.getWidth() / cols; // determines the chunk width and height  
+        int chunkHeight = image.getHeight() / rows;  
+        int count = 0;  
+        BufferedImage imgs[] = new BufferedImage[chunks]; //Image array to hold image chunks  
+        for (int x = 0; x < rows; x++) {  
+            for (int y = 0; y < cols; y++) {  
+                //Initialize the image array with image chunks  
+                imgs[count] = new BufferedImage(chunkWidth, chunkHeight, image.getType());  
+  
+                // draws the image chunk  
+                Graphics2D gr = imgs[count++].createGraphics();  
+                gr.drawImage(image, 0, 0, chunkWidth, chunkHeight, chunkWidth * y, chunkHeight * x, chunkWidth * y + chunkWidth, chunkHeight * x + chunkHeight, null);  
+                gr.dispose();  
+            }  
+        }  
+        System.out.println("Splitting done");  
+        
+        System.out.println(count);
+  
+  
+        //writing mini images into image files  
+        for (int i = 0; i < imgs.length; i++) {  
+        	switch(i)
+        	{
+            case 0:  suffix = "f";
+        	break;
+            case 1:  suffix = "r";
+        	break;
+            case 2:  suffix = "b";
+        	break;
+            case 3:  suffix = "l";
+        	break;
+     
+     
+        	
+        	
+        	}
+        	
+            ImageIO.write(imgs[i], "jpg", new File("d:/exif/images/","img_" + suffix + ".jpg"));  
+            
+        }  
+        System.out.println("Mini images created");  
+    }  
+}  
